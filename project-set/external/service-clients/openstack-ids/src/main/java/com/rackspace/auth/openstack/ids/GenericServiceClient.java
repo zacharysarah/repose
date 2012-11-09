91fcea61bddb79e1a8c207e902b541890b64f6b1
forcewhisperer <fran.torreslopez@rackspace.com>
2012-11-09 12:42:18 -0600

    Add code that allows repose to interface with SSL endpoints that have unverified certs.  This is used for debugging/testing when hitting a dev's local repose with another endpoint.

--- project-set/external/service-clients/openstack-ids/src/main/java/com/rackspace/auth/openstack/ids/GenericServiceClient.java
+++ project-set/external/service-clients/openstack-ids/src/main/java/com/rackspace/auth/openstack/ids/GenericServiceClient.java
@@ -1,5 +1,6 @@
 package com.rackspace.auth.openstack.ids;
 
+import javax.net.ssl.*;
 import javax.ws.rs.core.MediaType;
 
 import com.sun.jersey.api.client.Client;
@@ -8,6 +9,7 @@ import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
 import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
+import com.sun.jersey.client.urlconnection.HTTPSProperties;
 import com.sun.ws.rs.ext.RuntimeDelegateImpl;
 import org.openstack.docs.identity.api.v2.AuthenticationRequest;
 import org.openstack.docs.identity.api.v2.PasswordCredentialsRequiredUsername;
@@ -17,20 +19,26 @@ import com.rackspace.papi.commons.util.logging.jersey.LoggingFilter;
 import javax.ws.rs.ext.RuntimeDelegate;
 import javax.xml.bind.JAXBElement;
 import javax.xml.namespace.QName;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 /**
  * @author fran
  */
 public class GenericServiceClient {
-   private static final Logger LOG = LoggerFactory.getLogger(GenericServiceClient.class);
-   
+    private static final Logger LOG = LoggerFactory.getLogger(GenericServiceClient.class);
+
     static {
         // TODO: This should be removed, relocated or ignored. This is related to the issues we were seeing
         // where Jersey would work on some JVM installations but not all. This was rectified by adding a dependency
         // on jersey-server
-        
+
         RuntimeDelegate.setInstance(new RuntimeDelegateImpl());
     }
 
@@ -40,13 +48,26 @@ public class GenericServiceClient {
     public GenericServiceClient(String username, String password) {
         this.username = username;
         this.password = password;
-        
+
         DefaultClientConfig cc = new DefaultClientConfig();
         cc.getProperties().put(ClientConfig.PROPERTY_FOLLOW_REDIRECTS, false);
         // TODO: Eventually make these values configurable in Repose and implement
         // a "backoff" approach with logging.
         cc.getProperties().put(ClientConfig.PROPERTY_CONNECT_TIMEOUT, 30000);
         cc.getProperties().put(ClientConfig.PROPERTY_READ_TIMEOUT, 30000);
+
+        // ************************************************
+        // Only used for debug in this Repose version
+        // Unomment this out when testing and you
+        // don't care about SSL
+        // ************************************************
+//        allowAllServerCerts();
+//        try {
+//            cc.getProperties().put(HTTPSProperties.PROPERTY_HTTPS_PROPERTIES, new HTTPSProperties(new ReposeHostnameVerifier(), SSLContext.getDefault()));
+//        } catch (NoSuchAlgorithmException e) {
+//            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+//        }
+
         client = Client.create(cc);
 
         // TODO: Validate that this is required for all calls or only some
@@ -65,7 +86,7 @@ public class GenericServiceClient {
         credentials.setPassword(password);
 
         // TODO: These QNames should come from the schema objects themselves
-        JAXBElement jaxbCredentials = new JAXBElement(new QName("http://docs.openstack.org/identity/api/v2.0","passwordCredentials"), PasswordCredentialsRequiredUsername.class, credentials);
+        JAXBElement jaxbCredentials = new JAXBElement(new QName("http://docs.openstack.org/identity/api/v2.0", "passwordCredentials"), PasswordCredentialsRequiredUsername.class, credentials);
         AuthenticationRequest request = new AuthenticationRequest();
         request.setCredential(jaxbCredentials);
 
@@ -80,14 +101,61 @@ public class GenericServiceClient {
         WebResource resource = client.resource(uri);
 
         if (queryParameters.length % 2 != 0) {
-           throw new IllegalArgumentException("Query parameters must be in pairs.");
+            throw new IllegalArgumentException("Query parameters must be in pairs.");
         }
 
         for (int index = 0; index < queryParameters.length; index = index + 2) {
-           resource = resource.queryParam(queryParameters[index], queryParameters[index + 1]);
+            resource = resource.queryParam(queryParameters[index], queryParameters[index + 1]);
         }
 
         ClientResponse response = resource.header("Accept", "application/xml").header("X-Auth-Token", adminToken).get(ClientResponse.class);
         return new ServiceClientResponse(response.getStatus(), response.getEntityInputStream());
     }
+
+
+    //  In new Repose version this is much cleaner and is configurable.
+    //  But in order to debug locally this code is useful.  I get tired of
+    // checking it in and out so for now I'm going to leave this here in
+    // case you need it when debugging auth in staging or prod environments
+    // where SSL is used.
+
+    public void allowAllServerCerts() {
+        TrustManager[] trustAllCerts = new TrustManager[]{new TrustingX509TrustManager()};
+
+        // Install the all-trusting trust manager
+        try {
+            final SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new SecureRandom());
+            SSLContext.setDefault(sc);
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+            LOG.error("Problem creating SSL context: ", e);
+        }
+    }
+
+    private class TrustingX509TrustManager implements X509TrustManager {
+
+        @Override
+        public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return null;
+        }
+    }
+
+    private class ReposeHostnameVerifier implements HostnameVerifier {
+
+        @Override
+        public boolean verify(String hostname, SSLSession sslSession) {
+            LOG.info("verifying: " + hostname);
+            return true;
+        }
+    }
+
 }
