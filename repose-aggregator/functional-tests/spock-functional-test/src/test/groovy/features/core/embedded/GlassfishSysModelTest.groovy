7cbbd7e84ffa4313448b1e2c7639c9cd1cc1abee
Damien Johnson <damien.johnson@rackspace.com>
2014-04-02 10:53:50 -0500

    Initial commit

---
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/core/embedded/GlassfishSysModelTest.groovy
@@ -0,0 +1,68 @@
+package features.core.embedded
+
+import framework.*
+import org.rackspace.deproxy.Deproxy
+import spock.lang.Shared
+import spock.lang.Specification
+
+class GlassfishSysModelTest extends Specification {
+    static Deproxy deproxy
+    static ReposeLauncher repose
+    static String reposeGlassfishEndpoint
+
+    @Shared
+    def ReposeLogSearch logSearch
+
+    def setupSpec() {
+        def reposeClusterId = "repose"
+        def reposeNodeId = "node"
+        def reposeNodeHostname = "localhost"
+
+        def TestProperties properties = new TestProperties()
+
+        def configDirectory = properties.configDirectory
+        def configTemplates = properties.rawConfigDirectory
+        def rootWar = properties.reposeRootWar
+        def glassfishJar = properties.glassfishJar
+        def reposePort = properties.reposePort
+        def reposeShutdownPort = properties.reposeShutdownPort
+        def originPort = properties.targetPort
+        def logFile = properties.logFile
+
+        def params = properties.defaultTemplateParams
+        params += [
+                "repose.cluster.id": reposeClusterId,
+                "repose.node.id"   : reposeNodeId,
+                "repose.hostname"  : reposeNodeHostname
+        ]
+
+        reposeGlassfishEndpoint = "http://localhost:${reposePort}"
+
+        deproxy = new Deproxy()
+        deproxy.addEndpoint(originPort)
+
+        logSearch = new ReposeLogSearch(logFile)
+
+        ReposeConfigurationProvider config = new ReposeConfigurationProvider(configDirectory, configTemplates)
+
+        config.applyConfigs("common", params)
+        config.applyConfigs("features/core/embedded/badhost", params)
+
+        repose = new ReposeContainerLauncher(config, glassfishJar, reposeClusterId, reposeNodeId, rootWar,
+                reposePort, reposeShutdownPort)
+    }
+
+    def cleanupSpec() {
+        repose?.stop()
+        deproxy?.shutdown()
+    }
+
+    def "when the host cannot find itself in the system model, then Repose should not crash"() {
+        when:
+        repose.start()
+        repose.waitForNon500FromUrl(reposeGlassfishEndpoint, 120)
+
+        then:
+        logSearch.searchByString("NullPointerException").size() == 0
+    }
+}
