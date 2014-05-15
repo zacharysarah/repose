3559252f614ff773960f54888806b3da43be4677
jenny.vo <jennyvo@ubuntu.ubuntu-domain>
2014-05-15 15:19:03 -0500

    add test to replicate issue for atom with apivalidator filter and content body exceed limit

--- repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/apivalidator/CFApiValidatorContentLimitTest.groovy
+++ repose-aggregator/functional-tests/spock-functional-test/src/test/groovy/features/filters/apivalidator/CFApiValidatorContentLimitTest.groovy
@@ -8,9 +8,9 @@ import org.rackspace.deproxy.MessageChain
 import spock.lang.Unroll
 
 /**
- * Created by jennyvo on 5/9/14.
+ * Created by jennyvo on 5/15/14.
  */
-class ApiValidatorContentLimitTest extends ReposeValveTest{
+class CFApiValidatorContentLimitTest extends ReposeValveTest{
     String charset = (('a'..'z') + ('0'..'9')).join()
     def setupSpec() {
         deproxy = new Deproxy()
@@ -18,10 +18,11 @@ class ApiValidatorContentLimitTest extends ReposeValveTest{
 
         def params = properties.getDefaultTemplateParams()
         repose.configurationProvider.applyConfigs("common", params)
-        repose.configurationProvider.applyConfigs("features/filters/apivalidator/contentlimit", params)
+        repose.configurationProvider.applyConfigs("features/filters/apivalidator/cfcontentlimit", params)
         repose.start()
         repose.waitForNon500FromUrl(reposeEndpoint)
     }
+
     def setup () {
         def logSearch = new ReposeLogSearch(properties.logFile)
         logSearch.cleanLog()
@@ -34,43 +35,9 @@ class ApiValidatorContentLimitTest extends ReposeValveTest{
             deproxy.shutdown()
     }
 
-    @Unroll("When sending a #reqMethod through repose")
     def "should return 413 on request content larger than body limit"(){
 
         given: "I have a request body that exceed the header size limit"
-        //def body = RandomStringUtils.random(1024, charset)
-        def text = RandomStringUtils.random(768, charset)
-        def body = """<?xml version="1.0"?>
-                      <entry xmlns="http://www.w3.org/2005/Atom">
-                          <title>CF test</title>
-                          <author><name>JV</name></author>
-                          <content type="text">"""+ text +"""</content>
-                          <category term="cloudfeed" />
-                      </entry>"""
-        def headers = ["Content-Type": "application/atom+xml"]
-
-        when: "I send a request to REPOSE with my request body"
-        MessageChain mc = deproxy.makeRequest(url: reposeEndpoint + "/wadl/test/resource1/"+resourceId, headers: headers, requestBody: body, method: reqMethod)
-
-        then: "Reponse with 413"
-        mc.receivedResponse.code == "413"
-        mc.receivedResponse.message == "Request Entity Too Large"
-        mc.handlings.size() == 0
-
-
-        where:
-        reqMethod       | resourceId
-        "PUT"           |"updateEntry"
-        "POST"          |"createEntry"
-        "DELETE"        |"deteleEntry"
-        "PATCH"         |"modifyEntry"
-    }
-
-    @Unroll("When sending a #reqMethod through repose request content within body limit")
-    def "should return 200 on request content within body limit"(){
-
-        given: "I have a request body that exceed the header size limit"
-        //def body = RandomStringUtils.random(1021, charset)
         def text = RandomStringUtils.random(256, charset)
         def body = """<?xml version="1.0"?>
                       <entry xmlns="http://www.w3.org/2005/Atom">
@@ -79,21 +46,38 @@ class ApiValidatorContentLimitTest extends ReposeValveTest{
                           <content type="text">"""+ text +"""</content>
                           <category term="cloudfeed" />
                       </entry>"""
-        def headers = ["Content-Type": "application/atom+xml"]
+        def headers = ['Content-Type': 'application/atom+xml']
+        println body.length()
 
         when: "I send a request to REPOSE with my request body"
-        MessageChain mc = deproxy.makeRequest(url: reposeEndpoint + "/wadl/test/resource1/"+resourceId, headers: headers, requestBody: body, method: reqMethod)
+        MessageChain mc = deproxy.makeRequest(url: reposeEndpoint + "/perftest1/events", headers: headers, requestBody: body, method: "POST")
+
+        then: "Reponse with 413"
+        mc.receivedResponse.code == "413"
+        mc.receivedResponse.message == "Request Entity Too Large"
+        mc.handlings.size() == 0
+    }
+
+    def "should return 200 on request within content body limit"(){
+
+        given: "I have a request body that exceed the header size limit"
+        def text = RandomStringUtils.random(10, charset)
+        def body = """<?xml version="1.0"?>
+                      <entry xmlns="http://www.w3.org/2005/Atom">
+                          <title>CF test</title>
+                          <author><name>JV</name></author>
+                          <content type="text">"""+ text +"""</content>
+                          <category term="cloudfeed" />
+                      </entry>"""
+        println body.length()
+
+        def headers = ['Content-Type': 'application/atom+xml']
+
+        when: "I send a request to REPOSE with my request body"
+        MessageChain mc = deproxy.makeRequest(url: reposeEndpoint + "/perftest1/events", headers: headers, requestBody: body, method: "POST")
 
         then: "Reponse with 200"
         mc.receivedResponse.code == "200"
         mc.handlings.size() == 1
-
-
-        where:
-        reqMethod       | resourceId
-        "PUT"           |"updateEntry"
-        "POST"          |"createEntry"
-        "DELETE"        |"deteleEntry"
-        "PATCH"         |"modifyEntry"
     }
 }
